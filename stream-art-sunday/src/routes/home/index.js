d7e1c7ced52d56363eb9a5115cd79f50e81e11ff
briaguya <briaguya@alice>
2021-12-28 00:54:50 -0500

    trying using tmi.js, restructuring components

--- stream-art-sunday/src/routes/home/index.js
+++ stream-art-sunday/src/routes/home/index.js
@@ -1,71 +1,7 @@
 import { h } from 'preact';
+import tmi from 'tmi.js';
 import { useState, useReducer } from 'preact/hooks';
 import style from './style.css';
-
-// artColorString() {
-// 	switch (this.mode) {
-// 		case 1:
-// 			return 'hsl('+this.baseHue+',100%,75%)';
-// 		default:
-// 			return 'hsl('+this.baseHue+',100%,50%)';
-// 	}
-// },
-// sundayColorString() {
-// 	switch (this.mode) {
-// 		case 1:
-// 			return 'hsl('+(this.baseHue + 30)%360+',100%,75%)';
-// 		default:
-// 			return 'hsl('+(this.baseHue + 180)%360+',100%,50%)';
-// 	}
-// },
-// artText() {
-// 	if(this.endgame)
-// 		return 'SCORE';
-// 	return 'ART!';
-// },
-// sundayText() {
-// 	if(this.endgame)
-// 		return this.score;
-// 	return 'SUNDAY!';
-// }
-
-// <div id="art-sunday">
-// 	<div id="art" class="text">
-// 		<h1 style={`{ color: ${artColorString} }`}>{artText}</h1>
-// 	</div>
-// 	<div id="sunday" class="text">
-// 		<h1 style={`{ color: ${sundayColorString} }`}>{sundayText}</h1>
-// 	</div>
-// </div>
-
-
-// fadeSeven() {
-// 	this.baseHue = (this.baseHue + this.hueChangeRate) % 360
-// 	this.score += this.hueChangeRate
-// 	if (this.score % 570 === 0) {
-// 		this.mode = (this.mode + 1) % 2
-// 		switch (this.score) {
-// 			case 24510:
-// 				console.log('endgame')
-// 				this.endgame = true
-// 				break
-// 			case 17670:
-// 				this.hueChangeRate = 2
-// 				this.maxChangeRate = 2
-// 				break
-// 			case 9120:
-// 				this.hueChangeRate = 0.5
-// 				this.maxChangeRate = 2
-// 				break
-// 			case 5700:
-// 				this.hueChangeRate = 1
-// 				this.maxChangeRate = 0.5
-// 				break
-// 			default:
-// 				this.hueChangeRate += (this.maxChangeRate - this.hueChangeRate) / 2
-// 		}
-// 	}
-// }
 
 const levelRates = [0.5, 1, 0.75, 2, 1, 0.5];
 const levelPoints = [570, 570, 1140, 570, 5700, 1140];
@@ -74,23 +10,44 @@ Array.prototype.random = function () {
   return this[Math.floor((Math.random()*this.length))];
 }
 
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      baseHue: 0,
+      mode: 0,
+      currentLevel: 0,
+      pointsToNextLevel: 570,
+      currentHueChangeRate: 0.5,
+    }
+
+    const client = new tmi.Client({
+      channels: [ 'briaguya0' ]
+    });
+    client.connect();
+    client.on('message', (channel, tags, message, self) => {
+      // "Alca: Hello, World!"
+      console.log(`${tags['display-name']}: ${message}`);
+    });
+  }
+}
+
 const Home = () => {
-	const [count, setCount] = useState(0);
-	const [baseHue, setBaseHue] = useState(0);
-	const [mode, setMode] = useState(0);
-	const [currentLevel, setCurrentLevel] = useState(0);
-	const [pointsToNextLevel, setPointsToNextLevel] = useState(570);
-	const [currentHueChangeRate, setCurrentHueChangeRate] = useState(levelRates[0]);
-	const [nextHueChangeRate, setNextHueChangeRate] = useState(levelRates[1]);
+
+
+
+
+
+
 
 	const initialScore = 0;
 	const reducer = (score, reps) => {
 		setPointsToNextLevel(pointsToNextLevel - (currentHueChangeRate * reps))
 		if(!pointsToNextLevel) {
 			setMode((mode + 1) % 2);
-			setCurrentHueChangeRate(nextHueChangeRate);
 			setCurrentLevel(currentLevel + 1);
-			setNextHueChangeRate(levelRates.random());
+      setCurrentHueChangeRate(levelRates.random());
 			setPointsToNextLevel(levelPoints.random());
 		}
 		setBaseHue((baseHue + currentHueChangeRate * reps) % 360);
@@ -102,10 +59,10 @@ const Home = () => {
 		<div onMouseMove={() => dispatch(1)} class={style.home}>
 		<div id="art-sunday">
 			<div id="art" class="text">
-				<h1 style={{color: `hsl(${baseHue},100%,${mode ? '75%' : '50%'})` }}>{'ART'}</h1>
+				<h1 style={{color: `hsl(${baseHue},100%,${mode ? '75%' : '50%'})` }}>{`LEVEL: ${currentLevel}`}</h1>
 			</div>
 			<div id="sunday" class="text">
-				<h1 style={{color: `hsl(${mode ? baseHue + 30 : baseHue + 180},100%,${mode ? '75%' : '50%'})` }}>{'SUNDAY'}</h1>
+				<h1 style={{color: `hsl(${mode ? baseHue + 30 : baseHue + 180},100%,${mode ? '75%' : '50%'})` }}>{`SCORE: ${score}`}</h1>
 			</div>
 		</div>
 			<div>
